860f66733d59f45a28fc11b3d22428e6231a2ac0
ClintonYounge <clintonyounge18@gmail.com>
2023-08-14 16:42:58 -0400

    Add prerequisite code
    - Add menu structure

---
+++ item.rb
@@ -0,0 +1,18 @@
+class Item
+  def initialize(title = "unknown", genre = "unknown", author = "unknown", label = "unknown", publish_date = "unknown", archived = false, id: nil)
+    @genre = genre
+    @author = author
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+    @id = id || rand(1000)
+  end
+    
+  def can_be_archived?
+    @publish_date > "2013-01-01" ? true : false
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
