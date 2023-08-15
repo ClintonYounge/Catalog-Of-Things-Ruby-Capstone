044966c649bc5b51565b5878cd3a9bdbd896fc41
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-15 20:05:27 +0300

    Overirde can_be_archived class.

--- models/book.rb
+++ models/book.rb
@@ -10,4 +10,8 @@ class Book < Item
         @publisher = publisher
         @cover_state = cover_state
     end
+
+    def can_be_archived?()
+        super || (@cover_state.downcase == 'bad')
+    end
 end
