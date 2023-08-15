19d66d31ab1c8a08f7fea55f853395586233c7e2
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-15 15:15:14 -0400

    try to improve item and music classes

--- item.rb
+++ item.rb
@@ -1,33 +1,24 @@
+require 'securerandom'
+
 class Item
-  def initialize(publish_date)
-    @publish_date = publish_date
+  attr_accessor :label, :genre, :author
+  attr_reader :id, :published_date, :archived
+
+  def initialize(published_date:, label: 'Unknown', genre: 'Unknown', author: 'Unknown')
+    @id = SecureRandom.uuid
+    @label = label
+    @genre = genre
+    @author = author
+    @published_date = published_date
     @archived = false
-    @id = id || rand(1000)
+    move_to_archive
+  end
+
+  def can_be_archived?
+    (Time.now - DateTime.parse(@published_date).to_time) > 10 * 365 * 24 * 60 * 60 # 10 years in seconds
   end
 
   def move_to_archive
     @archived = can_be_archived?
-  end
-
-  def genre=(genre)
-    @genre = genre
-    genre.add_item(self)
-  end
-
-  def author=(author)
-    author.add_item(self)
-  end
-
-  def label=(label)
-    return if @label == label
-
-    @label = label
-    label&.add_item(self)
-  end
-
-  private
-
-  def can_be_archived?
-    @publish_date < (Time.now - (10 * 365 * 24 * 60 * 60)).strftime('%Y-%m-%d')
   end
 end
