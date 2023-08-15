11a2a7a0752a1b6ef75254caf4af521ff9cc8d1b
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-15 16:11:39 +0300

    Add add_item method

--- models/label.rb
+++ models/label.rb
@@ -1,9 +1,14 @@
 class label
     attr_reader :id, :title, :color, :items
     def initialize(title, color, id = nil)
-        @id = id || rand(1...10000)
+        @id = id || Random.rand(1...10000)
         @title = title
         @color = color    
         @items = []
+    end
+    def add_item(item)
+        return unless item.is_a?(Item) && !@items.include?(item)
+        @items << item
+        item.label = self
     end
 end
