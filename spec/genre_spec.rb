b83ef0a66d4da92c0b306c8a29c182ba8f88b68a
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-15 15:12:22 -0400

    autofix linter errors

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,16 +1,15 @@
 require_relative '../genre'
 
 RSpec.describe Genre do
-    let(:genre) { Genre.new(name: 'Rock') }
-  
-    describe '#add_item' do
-      it 'adds the item to the genre and sets the genre property of the item' do
-        item = double('Item')
-        expect(item).to receive(:genre=).with(genre)
-        
-        genre.add_item(item)
-        expect(genre.items).to include(item)
-      end
+  let(:genre) { Genre.new(name: 'Rock') }
+
+  describe '#add_item' do
+    it 'adds the item to the genre and sets the genre property of the item' do
+      item = double('Item')
+      expect(item).to receive(:genre=).with(genre)
+
+      genre.add_item(item)
+      expect(genre.items).to include(item)
     end
   end
-  
+end
