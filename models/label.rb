d1315ee39936a87e745ed1b6b1dc05547d4f341f
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-15 18:49:19 +0300

    Fix linter errors

--- models/label.rb
+++ models/label.rb
@@ -1,14 +1,17 @@
-class label
-    attr_reader :id, :title, :color, :items
-    def initialize(title, color, id = nil)
-        @id = id || Random.rand(1...10000)
-        @title = title
-        @color = color    
-        @items = []
-    end
-    def add_item(item)
-        return unless item.is_a?(Item) && !@items.include?(item)
-        @items << item
-        item.label = self
-    end
+class Label
+  attr_reader :id, :title, :color, :items
+
+  def initialize(title, color, id = nil)
+    @id = id || Random.rand(1...10_000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    return unless item.is_a?(Item) && !@items.include?(item)
+
+    @items << item
+    item.label = self
+  end
 end
