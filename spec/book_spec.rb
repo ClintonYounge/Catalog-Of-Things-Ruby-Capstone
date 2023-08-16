9b71bf59bb3bcf4436a3702525d8685e347f703c
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-08-17 00:18:39 +0300

    Fix linters

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,25 +1,24 @@
 require_relative '../models/book'
 
 describe Book do
-    before :each do
-        @book1 = Book.new(published_date: '2023-12-01', label: 'Ruby', publisher: 'Phoenix', cover_state: 'Good')
-        @book2 = Book.new(published_date: '2010-12-01', label: 'Ruby', publisher: 'Phoenix', cover_state: 'Good')
-        @book3 = Book.new(published_date: '2023-12-01', label: 'Ruby', publisher: 'Phoenix', cover_state: 'Bad')
+  before :each do
+    @book1 = Book.new(published_date: '2023-12-01', label: 'Ruby', publisher: 'Phoenix', cover_state: 'Good')
+    @book2 = Book.new(published_date: '2010-12-01', label: 'Ruby', publisher: 'Phoenix', cover_state: 'Good')
+    @book3 = Book.new(published_date: '2023-12-01', label: 'Ruby', publisher: 'Phoenix', cover_state: 'Bad')
+  end
+  describe '#can_be_archived?' do
+    it 'Returns false when date is <10years or when cover_state is good.' do
+      expect(@book1.can_be_archived?).to be false
     end
-    describe '#can_be_archived?' do
-        it 'Returns false when date is <10years or when cover_state is good.' do
-            expect(@book1.can_be_archived?).to be false
-        end
+  end
+  describe '#can_be_archived?' do
+    it 'Returns true when date is >10years or when cover_state is bad.' do
+      expect(@book2.can_be_archived?).to be true
     end
-    describe '#can_be_archived?' do
-        it 'Returns true when date is >10years or when cover_state is bad.' do
-            expect(@book2.can_be_archived?).to be true
-        end
+  end
+  describe '#can_be_archived?' do
+    it 'Returns true when date is <10years or when cover_state is bad.' do
+      expect(@book3.can_be_archived?).to be true
     end
-    describe '#can_be_archived?' do
-        it 'Returns true when date is <10years or when cover_state is bad.' do
-            expect(@book3.can_be_archived?).to be true
-        end
-    end
+  end
 end
-
