4fd789bc9c7765829b1ce22482efcd57ba983b11
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-08-16 22:59:10 -0400

    delete already deleted cases

--- spec/game_methods_spec.rb
+++ spec/game_methods_spec.rb
@@ -7,12 +7,6 @@ describe GameMethods do
       games = [Game.new('Yes', Date.new(2019, 1, 1))]
       expect { game_methods.list_games(games) }.to output("Multiplayer: Yes. Last played at: 2019-01-01\n").to_stdout
     end
-
-    it 'should return a message if no games are found' do
-      game_methods = GameMethods.new
-      games = []
-      expect { game_methods.list_games(games) }.to output("No games found\n").to_stdout
-    end
   end
 
   describe '#list_authors' do
@@ -20,12 +14,6 @@ describe GameMethods do
       game_methods = GameMethods.new
       authors = [Author.new('John', 'Doe')]
       expect { game_methods.list_authors(authors) }.to output("First Name: John, Last Name: Doe\n").to_stdout
-    end
-
-    it 'should return a message if no authors are found' do
-      game_methods = GameMethods.new
-      authors = []
-      expect { game_methods.list_authors(authors) }.to output("No authors found\n").to_stdout
     end
   end
 
